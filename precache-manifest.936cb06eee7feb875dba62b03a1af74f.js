3e2cb96b7bddc6f8be77dfc0380f05217a53136c
Rishabh <baidrishabh443@gmail.com>
2020-09-19 17:42:45 +0530

    Updates

---
+++ precache-manifest.936cb06eee7feb875dba62b03a1af74f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61551809157b8c08a67cd786ee2f186",
+    "url": "/TaskBar/index.html"
+  },
+  {
+    "revision": "74fd82bc7fbeebeb2c87",
+    "url": "/TaskBar/static/css/main.77d36974.chunk.css"
+  },
+  {
+    "revision": "aac7be8c04f3be0ded9c",
+    "url": "/TaskBar/static/js/2.80b3db9d.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/TaskBar/static/js/2.80b3db9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74fd82bc7fbeebeb2c87",
+    "url": "/TaskBar/static/js/main.171e081d.chunk.js"
+  },
+  {
+    "revision": "cb04247e429fab08ee03",
+    "url": "/TaskBar/static/js/runtime-main.c3492f33.js"
+  },
+  {
+    "revision": "e128286076c2ab9c8716d9eff234171a",
+    "url": "/TaskBar/static/media/White_Zombie.e1282860.svg"
+  }
+]);
